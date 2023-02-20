0d238251507bffcf0a2f40b89715bc23505ba458
Tseren Ochir <ochiroe@users.noreply.github.com>
2023-02-20 11:27:35 +0800

    Update createCA.sh

--- createCA.sh
+++ createCA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p 'Enter Output CA Filename without Extension: ' caFileName
-read -p 'Enter CA Name (eg. My Home):' CANAME
+read -p 'Enter CA Name:' CANAME
 printf "\n\n (+) Generating CA Key"
 #for some reason windows 10 displays password on prompt, so hiding.. 
 stty -echo
@@ -8,5 +8,5 @@ openssl genrsa -des3 -out ${caFileName}.key 2048
 stty echo
 printf "\n\n (+) Generating Root Certificate"
 stty -echo
-openssl req -x509 -new -nodes -key ${caFileName}.key -sha256 -days 1825 -subj "/C=US/ST=Arizona/L=Phoenix/CN=${CANAME}" -out ${caFileName}.pem
+openssl req -x509 -new -nodes -key ${caFileName}.key -sha256 -days 1096 -subj "/C=MN/L=Ulaanbaatar/CN=${CANAME}" -out ${caFileName}.pem
 stty echo
